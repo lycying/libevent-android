67d651050b79b07f5f478f3040ca08ff31796264
lycying <lycying@gmail.com>
2016-03-03 22:33:15 +0800

    init

---
+++ build.sh
@@ -0,0 +1,24 @@
+rm -rvf jni/libevent-2.0.22-stable/
+tar xvf libevent-2.0.22-stable.tar.gz -C jni/
+cd jni/libevent-2.0.22-stable/
+$NDK/build/tools/make-standalone-toolchain.sh --platform=android-14 --toolchain=arm-linux-androideabi-4.9 --install-dir=`pwd`/android-toolchain-arm
+export TOOLCHAIN_PATH=`pwd`/android-toolchain-arm/bin
+export TOOL=arm-linux-androideabi
+export NDK_TOOLCHAIN_BASENAME=${TOOLCHAIN_PATH}/${TOOL}
+export CC=$NDK_TOOLCHAIN_BASENAME-gcc
+export CXX=$NDK_TOOLCHAIN_BASENAME-g++
+export LINK=${CXX}
+export LD=$NDK_TOOLCHAIN_BASENAME-ld
+export AR=$NDK_TOOLCHAIN_BASENAME-ar
+export RANLIB=$NDK_TOOLCHAIN_BASENAME-ranlib
+export STRIP=$NDK_TOOLCHAIN_BASENAME-strip
+export ARCH_FLAGS="-march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16"
+export ARCH_LINK="-march=armv7-a -Wl,--fix-cortex-a8"
+export CPPFLAGS=" ${ARCH_FLAGS} -fpic -ffunction-sections -funwind-tables -fstack-protector -fno-strict-aliasing -finline-limit=64 "
+export CXXFLAGS=" ${ARCH_FLAGS} -fpic -ffunction-sections -funwind-tables -fstack-protector -fno-strict-aliasing -finline-limit=64 -frtti -fexceptions "
+export CFLAGS=" ${ARCH_FLAGS} -fpic -ffunction-sections -funwind-tables -fstack-protector -fno-strict-aliasing -finline-limit=64 "
+export LDFLAGS=" ${ARCH_LINK} "
+./configure --host=arm-linux-androideabi
+make
+cd ..
+ndk-build
---
+++ jni/Android.mk
@@ -0,0 +1,62 @@
+LOCAL_PATH := $(call my-dir)
+
+XLIBDIR := $(LOCAL_PATH)/libevent-2.0.22-stable/.libs
+
+include $(CLEAR_VARS)
+
+
+LOCAL_MODULE    := event
+LOCAL_SRC_FILES := $(XLIBDIR)/libevent.a
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+LOCAL_MODULE_SUFFIX := .a
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := event_core
+LOCAL_SRC_FILES := $(XLIBDIR)/libevent_core.a
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+LOCAL_MODULE_SUFFIX := .a
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := event_extra
+LOCAL_SRC_FILES := $(XLIBDIR)/libevent_extra.a
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+LOCAL_MODULE_SUFFIX := .a
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := event_pthreads
+LOCAL_SRC_FILES := $(XLIBDIR)/libevent_pthreads.a
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+LOCAL_MODULE_SUFFIX := .a
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+
+# This is the target being built.
+LOCAL_MODULE := demo_libevent
+
+# All of the source files that we will compile.
+LOCAL_SRC_FILES := demo_libevent.c
+
+LOCAL_STATIC_LIBRARIES := event event_core event_extra event_pthreads
+
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH) \
+    $(LOCAL_PATH)/libevent-2.0.22-stable/include
+
+LOCAL_LDLIBS := -llog
+
+include $(BUILD_EXECUTABLE)
